1efb073bac62576f14934fb9d3675265d28060e0
Lucas Justino <lucas.justino.dev@gmail.com>
2023-12-06 00:53:58 -0300

    Criação da Base da Calculadora

--- script.js
+++ script.js
@@ -1,1 +1,56 @@
-console.log("Eu to com sonoooo"); 
+function lerNumero(){
+    let valor = prompt("Digite o valor:");
+
+    while(isNaN(valor) || valor === "" || valor.trim() === ""){ //VERIFICAÇÃO PARA ACEITAR APENAS NÚMEROS, O TRIM USADO PARA REMOVER TODOS OS ESPAÇOS DA ENTRADA
+        valor = prompt("Valor incorreto! Digite novamente:")
+    }
+    return valor;
+}
+
+function lerOperacao(){
+    let operacao = prompt("Digite a operação: '+', '-', '*', '/', '%', 'LOG', 'RAIZ'");
+    const operacoesValidas = ['+', '-', '*', '/', '%', 'LOG', 'RAIZ'];
+
+    while (!operacoesValidas.includes(operacao)) { //SE ACASO A OPERACAO LIDA NÃO ESTIVER DENTRO DAS OPERAÇÕES VÁLIDAS, REPETE A LEITURA DA OPERAÇÃO
+        operacao = prompt("Operação inválida! Digite novamente:");
+    }
+
+    return operacao;
+}
+
+function calcular(){
+    let numero1 = lerNumero();
+    let operacao = lerOperacao();
+    let numero2 = lerNumero();
+    let resultado;
+    
+    switch(operacao){
+        case "+":
+
+            break;
+        case "-":
+
+            break;
+        case "*":
+
+            break;
+        case "/":
+
+            break;
+        case "%":
+
+            break;
+        case "LOG":
+
+            break;
+        case "RAIZ":
+
+            break;
+        default:
+            console.log("Operacao Inválida");
+            break;
+    }
+    return resultado;
+}
+
+console.log(calcular());
